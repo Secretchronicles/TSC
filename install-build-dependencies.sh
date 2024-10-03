897bef014d34083206f3ce39005f271f5ba3fb47
Lauri Ojansivu <x@xet7.org>
2024-10-04 00:06:06 +0300

    Added glm development headers, that are required now to build TSC.
    Thanks to Quintus !

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -6,7 +6,7 @@
 sudo apt-get -y install ruby-full rake gperf pkg-config bison libglew-dev \
   freeglut3-dev gettext libpng-dev libpcre3-dev libxml++2.6-dev \
   libfreetype6-dev libdevil-dev libboost-all-dev libsfml-dev \
-  libcegui-mk2-dev cmake build-essential git git-core
+  libcegui-mk2-dev cmake build-essential git git-core libglm-dev
 
 # Install Ruby gems
 sudo gem install bundler nanoc adsf kramdown coderay
