62a6d9f00bbce596693fb6fefd236647c07b0a02
Lauri Ojansivu <x@xet7.org>
2024-10-04 00:08:54 +0300

    Not installing CEGUI from disto repos. Using newest that Quintus added.
    Thanks to Quintus !

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -6,7 +6,11 @@
 sudo apt-get -y install ruby-full rake gperf pkg-config bison libglew-dev \
   freeglut3-dev gettext libpng-dev libpcre3-dev libxml++2.6-dev \
   libfreetype6-dev libdevil-dev libboost-all-dev libsfml-dev \
-  libcegui-mk2-dev cmake build-essential git git-core libglm-dev
+  cmake build-essential git git-core libglm-dev
+
+# Quintus added newer CEGUI 2024-10-04, so this is not installed anymore:
+#  libcegui-mk2-dev
+
 
 # Install Ruby gems
 sudo gem install bundler nanoc adsf kramdown coderay
